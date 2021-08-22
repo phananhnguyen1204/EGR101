c3f159d8f99c04e2ce30171cb1b4533db999e81e
Phan Anh <phananh12042003@gmail.com>
2021-08-22 01:02:06 -0400

    add class07.py

--- Inclass/class07/class07.py
+++ Inclass/class07/class07.py
@@ -45,90 +45,147 @@
 #2. Logout
 #If user choose 1 return to ask see events or see orgs or create events
 #If user choose 2 return to ask use program or stop programs
-
-def sign_in(account):
-
-    print("""
-    1. Use program
-    2. Stop program
-    """)
-    use_program=int(input(''))
-    while(use_program==1):
-        print("""
-        1. User
-        2. Orgination
-        """)
-        user_or_org=int(input('User or org? '))
-        if user_or_org==1:
-            print("""
-            1. Sign in 
-            2. Sign up
-            """)
-            sign_in_or_sign_up=int(input())
-            if sign_in_or_sign_up==1:
-                
-                while(True):
-                    email=input("")
-                    password=input("")
-                    if email not in account:
-                        print("Your account does not exit")
-                    elif password!=account(email):
-                        print("Your password is not correct")
-                    else:
-                        break
+def sign_in(accounts):
+    while(True):
+        email=input('Enter your email: ')
+        password=input('Enter your password: ')
+        if email not in accounts:
+            print('Your account doese not exist. Please try again!')
+        else:
+            if accounts[email]!=password:
+                print('Your password is not correct. Please try again!')
             else:
-                while(True):
-                    email=input("")
-                    password=input("")
-                    confirm=input("")
-                    if email in account:
-                        print("Your email has been used")
-                    elif password==account(email):
-                        print("Your password has been used")
-                    elif confirm!=password:
-                        print("Your password doesn't match")
-                    else:
-                        account[email]=password
-                        break
-    def user_actions(events, des_events, orgs, des_orgs):
-        print("""
-        1. See events
-        2. See orgs
-        """)
-        option=input()
-        if(option=="1"):
-            for i in range(0, len(events)):
-                print("{0}, {1}".format(i,events[i]))
-            number=int(input())
-            repeat=True
-            while repeat:
-                if number>=0 and number<len(des_events):
-                    print(des_events[number])
-                    repeat=False
-        if(option=="2"):
-            for i in range(0, len(orgs)):
-                print("{0}, {1}".format(i,orgs[i]))
-            number=int(input())
-            repeat=True
-            while repeat:
-                if number>=0 and number<len(des_orgs):
-                    print(des_orgs[number])
-                    repeat=False
-    def create_event(events,des_event):
-        while(True):
-            num=int(input("number of events: "))
-            if num<=5:
                 break
+def sign_up(accounts):
+    while(True):
+        while(True):
+            email=input('Enter your email: ')
+            if email in accounts:
+                print('Your email exited. Please try another email.')
             else:
-                print('Your numb is bigger than 5')
-        for i in range(1,num+1):
-            new_event=input()
-            new_des_event=input()
-            events.append(new_event)
-            des_event.append(new_des_event)
+                break
+        password=input('Create your password: ')
+        confirm=input('Confirm your password: ')
+        if confirm!=password:
+            print('Your password does not match. Please try again!')
+        else:
+            accounts[email]=password
+            
+            break
+accounts={'phananh12042003':'khongmatkhau','pinkmay2005':'123','khanhha':'hello'}
+
+def org_event(events,des_events,orgs,des_orgs):
+    while(True):
+        option1=input('See events, orgs, or create new events?(1, 2, or 3): ')
+        if option1=="1":
+            for i in range(0,len(events)):
+                print( '{0}. {1}'.format((i+1),events[i]))
+            numb=int(input('Choose event you wanna see a description '))
+            if numb>0 and numb<=len(des_events):
+                print(des_events[numb-1])
+        elif option1=='2':
+            for i in range(0,len(orgs)):
+                print( '{0}. {1}'.format((i+1),orgs[i]))
+            numb=int(input('Choose org you wanna see a description '))
+            if numb>0 and numb<=len(des_orgs):
+                print(des_orgs[numb-1])
+        else:
+            repeat=True
+            while(repeat==True):
+                numb=int(input('How many events do you wanna create? '))
+                if numb>5:
+                    print('Your number is out of range. Please try again!')
+                    break
+                else:
+                    
+                    for x in range(1,numb+1):
+                        name=input('Name of event:{0}, '.format(x))
+                        des=input('Description of event:{0} '.format(x))
+                        events.append(name)
+                        des_events.append(des)
+                    repeat=False
+                    
+
+
+        print(
+    """
+    1. Continue to see 
+    2. Logout
+    """
+        )
+        option2=input('Continue or logout?(1 or 2): ')
+        if option2=='2':
+            break
+    
+def user_event(events,des_events,orgs,des_orgs):
+    while(True):
+        option1=input('See events or orgs(1 or 2): ')
+        if option1=="1":
+            for i in range(0,len(events)):
+                print( '{0}. {1}'.format((i+1),events[i]))
+            numb=int(input('Choose event you wanna see a description '))
+            if numb>0 and numb<=len(des_events):
+                print(des_events[numb-1])
+        else:
+            for i in range(0,len(orgs)):
+                print( '{0}. {1}'.format((i+1),orgs[i]))
+            numb=int(input('Choose org you wanna see a description '))
+            if numb>0 and numb<=len(des_orgs):
+                print(des_orgs[numb-1])
+        print(
+    """
+    1. Continue to see 
+    2. Logout
+    """
+        )
+        option2=input('Continue or logout?(1 or 2): ')
+        if option2=='2':
+            break
         
 
-                    
+
+
+events=['Vietcode','Trung thu','Summer Camp']
+des_events=['vui','thieu nhi','rat vui']
+orgs=['Vietcode','Ecoway','Azure','Fagether']
+des_orgs=['code','moi truong','children','people']
+
+
+
+
+
+
+while(True):
+    print("""
+    #1. Use program
+    #2. Stop program
+    """)
+    use_program=input('Choose your option(1 or 2): ')
+    if use_program=="2":
+        break
+    else:
+        user_action=input('Sign in or sign up(1 or 2): ')
+        if user_action=='1':
+            sign_in(accounts)
+        else:
+            sign_up(accounts)
+        print(    
+    """
+1. User
+2. Orgination
+""")
+        role=input('Choose your role(1 or 2): ')
+        if role=="1":
+            print(
+"""
+1. See events
+2. See orgs            
+""")
+            user_event(events,des_events,orgs,des_orgs)
+        else:
+            org_event(events,des_events,orgs,des_orgs)
+
+
 
                 
 
